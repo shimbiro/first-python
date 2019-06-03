fec41da93dfb946b790c5bd90df3f81686e6ab0f
Shazz <shimbirosharon@gmail.com>
2019-06-03 19:07:20 -0400

    first python

---
+++ shell.py
@@ -0,0 +1,8 @@
+Python 3.6.7 (default, Oct 22 2018, 11:32:17) 
+[GCC 8.2.0] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================ RESTART: /home/shazz/Desktop/python/file.py ================
+Enter a temperature in Celsius:20
+In Fahrenheit, that is 68.0
+>>> 
